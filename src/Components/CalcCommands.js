51688c476d271f0e57a335c3633cf91ddddf39e1
Gh-Mensah <mkwaku055@gmail.com>
2022-02-12 18:02:22 +0000

    Styling app

--- src/Components/CalcCommands.js
+++ src/Components/CalcCommands.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
+import './commandcalc.css'
 
 
 class CalcCommands extends Component {
   render() {
     return (
-      <div>
-          CalcCommands
+      <div className='d-flex w-100 mx-auto border p-5 text-white bg-info' id='commandCont'>
+        <div className='d-inline-flex flex-column flex-fill  me-2'>
+        <span className='p-2 border w-100 text-center'>AC</span>
+        </div>
+        <div className='d-inline-flex flex-column flex-fill me-2'>
+          <span className='p-2 border w-100 mb-2 text-center'>(</span>
+          <span className='p-2 border w-100 mb-2 text-center'>7</span>
+          <span className='p-2 border w-100 mb-2 text-center'>4</span>
+          <span className='p-2 border w-100 mb-2 text-center'>1</span>
+          <span className='p-2 border w-100  text-center'><sup>+</sup>/<sub>-</sub></span>
+        </div>
+        <div className='d-inline-flex flex-column flex-fill me-2'>
+          <span className='p-2 border w-100 mb-2 text-center'>)</span>
+          <span className='p-2 border w-100 mb-2 text-center'>8</span>
+          <span className='p-2 border w-100 mb-2 text-center'>5</span>
+          <span className='p-2 border w-100 mb-2 text-center'>2</span>
+          <span className='p-2 border w-100  text-center'>0</span>
+        </div>
+        <div className='d-inline-flex flex-column  flex-fill me-2'>
+          <span className='p-2 border w-100 mb-2 text-center'>%</span>
+          <span className='p-2 border w-100 mb-2 text-center'>9</span>
+          <span className='p-2 border w-100 mb-2 text-center'>6</span>
+          <span className='p-2 border w-100 mb-2 text-center'>3</span>
+          <span className='p-2 border w-100  text-center'>.</span>
+        </div>
+        <div className='d-inline-flex flex-column  flex-fill me-2'>
+          <span className='p-2 border w-100 mb-2 text-center'>÷</span>
+          <span className='p-2 border w-100 mb-2 text-center'>×</span>
+          <span className='p-2 border w-100 mb-2 text-center'>-</span>
+          <span className='p-2 border w-100 mb-2 text-center'>+</span>
+          <span className='p-2 border w-100  text-center'>=</span>
+        </div>
+        <div className=' d-none d-md-inline-flex flex-column flex-fill '>
+        <span className='p-2 border w-100 h-100  border text-center'></span>
+        </div>
       </div>
     )
   }
--- src/Components/CalcDisplay.js
+++ src/Components/CalcDisplay.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function CalcDisplay() {
   return (
-  <div>
-      CalcDisplay
+  <div className='mb-3'>
+    <div className='h-100 w-100 mx-auto bg-info text-center text-white h2 fw-bolder'>Calculator</div>
+    <div className='h-100  mt-5 fs-1 text-end pe-5 me-4 fw-bold'>5x3</div>
+    <div className='h-100  mt-4 fs-3 text-end pe-5  me-4 opacity-50 fw-light'>15</div>
   </div>
   )
 }
